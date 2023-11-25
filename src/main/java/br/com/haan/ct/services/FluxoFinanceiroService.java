ff01a5d0ccf762a97cf5e07930740d386aac4504
Rian <Rian@Rian.mshome.net>
2023-11-25 18:06:19 -0300

    BE012 - criar controle para mostrar o lucro agrupado por mês

--- src/main/java/br/com/haan/ct/services/FluxoFinanceiroService.java
+++ src/main/java/br/com/haan/ct/services/FluxoFinanceiroService.java
@@ -62,6 +62,7 @@ public class FluxoFinanceiroService {
             return false;
         }
     }
+    
     public double lucroPorMes(int mes) {
         return fluxoFinanceiroRepository.lucroPorMes(mes);
     }
