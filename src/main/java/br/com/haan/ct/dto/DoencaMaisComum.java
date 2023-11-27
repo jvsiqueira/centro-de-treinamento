6269dd3d225c2e938c8dcb0fe2d54cbd00b1a59b
Lív Miranda <liviaraquelmiranda@gmail.com>
2023-11-27 11:32:03 -0300

    BE011 - criar controle para mostrar as doenças que mais ocorrem nos alunos

---
+++ src/main/java/br/com/haan/ct/dto/DoencaMaisComum.java
@@ -0,0 +1,7 @@
+package br.com.haan.ct.dto;
+
+public interface DoencaMaisComum {
+    public String getNomeDoenca();
+    public void setNomeDoenca(String nomeDoenca);
+    
+}
--- src/main/java/br/com/haan/ct/repositories/DoencaRepository.java
+++ src/main/java/br/com/haan/ct/repositories/DoencaRepository.java
@@ -1,8 +1,14 @@
 package br.com.haan.ct.repositories;
 
+import br.com.haan.ct.dto.DoencaMaisComum;
 import br.com.haan.ct.entities.Doenca;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface DoencaRepository extends CrudRepository<Doenca,Long >{
-    
+    @Query("SELECT doenca.descricao, COUNT(*) AS Comum FROM doenca JOIN alunodoenca ON alunodoenca.doenca_id = doenca.id GROUP BY doenca.id ORDER BY Comum DESC LIMIT 1")
+
+    public Iterable<DoencaMaisComum> nomeDoencaMaiscomum();
 }
+
